198432a3e846d0d632f430144f3a0ce0465d61a5
Joshua Paul <joshuapaul@Joshuas-MacBook-Pro.local>
2018-03-09 08:39:17 -0800

    Rename sub-cue to cue-card.

--- tests/integration/components/cue-card.js
+++ tests/integration/components/cue-card.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | sub-cue', function(hooks) {
+module('Integration | Component | cue-card', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{sub-cue}}`);
+    await render(hbs`{{cue-card}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#sub-cue}}
+      {{#cue-card}}
         template block text
-      {{/sub-cue}}
+      {{/cue-card}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
